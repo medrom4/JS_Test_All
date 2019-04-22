4844b9acf376a157e454f5091e3fccc02d8dd39f
medrom4 <medikroma@gmail.com>
2019-04-22 23:52:51 +0300

    innerHTML_outerHTML

--- beginner/script_my/script.js
+++ beginner/script_my/script.js
@@ -1,55 +1,14 @@
-function plus() {
-	let num1, num2, result;
+function out() {
+	let block = document.getElementById('out');
+	//	block.innerHTML += " - <div>текст должен добавиться!</div>"
+	// .innerHTML - вставляет всё
 
-	num1 = document.getElementById('n1').value;
-	num1 = parseInt(num1);
+	//	block.insertAdjacentHTML('afterEnd', ' вставляемый текст ');
+	//beforeBegin - перед открывающимся тегом
+	//afterBegin - после открывающего тега
+	//beforeEnd - перед закрывающимся тегом
+	//afterEnd - после закрывающего тега
 
-	num2 = document.getElementById('n2').value;
-	num2 = parseInt(num2);
+	block.outerHTML += '<div class="one"> вставляемый текст </div>';
 
-	result = num1 + num2;
-
-	document.getElementById('out').innerHTML = 'Результат: ' + result;
-}
-
-function minus() {
-	let num1, num2, result;
-
-	num1 = document.getElementById('n1').value;
-	num1 = parseInt(num1);
-
-	num2 = document.getElementById('n2').value;
-	num2 = parseInt(num2);
-
-	result = num1 - num2;
-
-	document.getElementById('out').innerHTML = 'Результат: ' + result;
-}
-
-function umnojenie() {
-	let num1, num2, result;
-
-	num1 = document.getElementById('n1').value;
-	num1 = parseInt(num1);
-
-	num2 = document.getElementById('n2').value;
-	num2 = parseInt(num2);
-
-	result = num1 * num2;
-
-	document.getElementById('out').innerHTML = 'Результат: ' + result;
-}
-
-function delenie() {
-	let num1, num2, result;
-
-	num1 = document.getElementById('n1').value;
-	num1 = parseInt(num1);
-
-	num2 = document.getElementById('n2').value;
-	num2 = parseInt(num2);
-
-	result = num1 / num2;
-
-	document.getElementById('out').innerHTML = 'Результат: ' + result;
 }
