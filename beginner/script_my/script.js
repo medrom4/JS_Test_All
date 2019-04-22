da30936b158b2726df6818748e9bba3f13bb0c63
medrom4 <medikroma@gmail.com>
2019-04-23 00:37:20 +0300

    while

--- beginner/script_my/script.js
+++ beginner/script_my/script.js
@@ -1,7 +1,22 @@
 function f1() {
 	let oneBl = document.getElementById('out');
+	let oneB2 = document.getElementById('out1');
 
-	for (var i = 0; i < 100; i++) {
+	//	let num1 = document.getElementById('n1').value;
+	//	num1 = parseInt(num1);
+
+	let i = 0;
+
+	while (i < 50) {
 		oneBl.innerHTML += i + ' ';
+		i++;
 	}
+
+	let j = 100;
+
+	do {
+		oneB2.innerHTML += j + ' ';
+		j -= 2;
+	}
+	while (j > 0);
 }
