20bbfc0fef2e77f5c8c4f3b540fb19f130f1303f
medrom4 <medikroma@gmail.com>
2019-04-24 23:53:17 +0300

    slider-OK

--- beginner/script_my/script.js
+++ beginner/script_my/script.js
@@ -1,30 +1,13 @@
-let arr = [];
-let arr1 = ['one', 'two', 23];
-arr1[3] = 'privet medved';
+document.getElementById('slider-left').onclick = sliderLeft;
+var left = 0;
 
-//for (var i = 0; i < arr1.length; i++) {
-//	document.getElementById('out').innerHTML += arr1[i] + '<br>';
-//}
+function sliderLeft() {
+	let polosa = document.getElementById('polosa');
+	left = left - 256;
 
-function masOut() {
-	let p = document.getElementById('out');
-	let str = '';
-
-	for (var i = 0; i < arr1.length; i++) {
-		str += i + ' -- ' + arr1[i] + '<br>';
+	if (left < -768) {
+		left = 0;
 	}
-	p.innerHTML = str;
-}
-masOut();
 
-
-function p1() {
-	let i1 = document.getElementById('i1').value;
-	arr1.push(i1);
-	masOut();
-}
-
-function p2() {
-	arr1.pop();
-	masOut();
+	polosa.style.left = left + 'px';
 }
