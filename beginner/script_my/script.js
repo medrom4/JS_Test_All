a328a634f7eef1679ce60bfa8648d5a7b8ec6674
medrom4 <medikroma@gmail.com>
2019-04-24 22:31:31 +0300

    array-go

--- beginner/script_my/script.js
+++ beginner/script_my/script.js
@@ -1,22 +1,3 @@
 function f1() {
-	let oneBl = document.getElementById('out');
-	let oneB2 = document.getElementById('out1');
 
-	//	let num1 = document.getElementById('n1').value;
-	//	num1 = parseInt(num1);
-
-	let i = 0;
-
-	while (i < 50) {
-		oneBl.innerHTML += i + ' ';
-		i++;
-	}
-
-	let j = 100;
-
-	do {
-		oneB2.innerHTML += j + ' ';
-		j -= 2;
-	}
-	while (j > 0);
 }
