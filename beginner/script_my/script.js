2694552b0b90f25f72479253c0f2a39a4ed49e83
medrom4 <medikroma@gmail.com>
2019-04-28 23:54:16 +0300

    array-summa

--- beginner/script_my/script.js
+++ beginner/script_my/script.js
@@ -1,18 +1,25 @@
-var m = {};
-var n = {
-	"one": 12,
-	"hello": "world",
-	"prim": 2000,
-	"dooble key": 777
+var man = {
+	"name": "Alex",
+	"age": 67,
+	"id": "2000123",
+	"sex": "male",
+	"year": function () {
+		return 2019 - this.age;
+	}
 };
 
-console.log(n["one"] + ' - ' + n.hello);
+console.log(man.year());
 
 
 ///////////////////////////////
-
-var out = document.getElementById('out');
-
-for (var key in n) {
-	out.innerHTML += key + ' - ' + n[key] + '<br>';
+var m = {
+	"mas": [5, 6, 34, 12, 45],
+	"sum": function () {
+		var sum = 0;
+		for (var i = 0; i < this.mas.length; i++) {
+			sum += this.mas[i];
+		}
+		return sum;
+	}
 }
+console.log(m.sum());
