820c86ee2e41473c466791721925f6f037b1c3bf
medrom4 <medikroma@gmail.com>
2019-04-29 01:44:21 +0300

    js-function

--- beginner/script_my/script.js
+++ beginner/script_my/script.js
@@ -1,29 +1,12 @@
-var goods = {
-
-	"28292": {
-		"name": "Банан",
-		"cost": 30,
-		"img": "https://cdn4.iconfinder.com/data/icons/materia-flat-fruits/24/027_045_banana_banan_plant_herb_fruit_food-256.png",
-		"sclad": "да"
-	},
-
-	"28293": {
-		"name": "Помидор",
-		"cost": 12,
-		"img": "https://cdn1.iconfinder.com/data/icons/Hooligans_Icons/250/Tomato.png",
-		"sclad": "нет"
-	}
-
-};
-
-console.log(goods);
-
-var out = '';
-for (var key in goods) {
-	out += 'Название: ' + goods[key].name + '<br>' +
-		'Цена: ' + goods[key].cost + '<br>' +
-		'Картинка: ' + '<img src="' + goods[key].img + '">' + '<br>' +
-		'Наличие на складе: ' + goods[key].sclad + '<br>' + '<hr>';
+function summa(a, b) {
+	a = a || 12;
+	b = b || 18;
+	alert(a + b)
+//		return (a + b);
 }
 
-document.getElementById('out').innerHTML = out;
+//console.log(summa(3));
+
+document.getElementById('b1').onclick = () => {
+	summa();
+}
