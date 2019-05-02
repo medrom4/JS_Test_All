2a3bbd5e547d216514f8cc134dc822ee428e223e
medrom4 <medikroma@gmail.com>
2019-05-02 15:04:37 +0300

    onkeypress

---
+++ beginner/script_my/script.js
@@ -0,0 +1,16 @@
+//document.onkeypress = (event) => {
+//    console.log(event);
+//    
+//    if(event.shiftKey) {
+//        console.log('Нажата клавиша Shift');
+//    }
+//}
+
+document.getElementById('test').onkeypress = function (event) {
+    //    console.log(event);
+
+    if (event.keyCode < 48 || event.keyCode > 57) {
+        console.log("Воодите цыфры!");
+        return false;
+    }
+}
