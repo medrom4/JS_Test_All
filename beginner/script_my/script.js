d9aab528788c594bc97223b3fab3c3e409fa361e
medrom4 <medikroma@gmail.com>
2019-05-02 14:20:56 +0300

    setInterval-setTimeout

--- beginner/script_my/script.js
+++ beginner/script_my/script.js
@@ -1,1 +1,33 @@
+var sdvig = 0;
+var test = document.getElementById('test');
+var timer;
 
+function move() {
+    test.style.marginLeft = sdvig + 'px';
+    sdvig += 10;
+    timer = setTimeout(move, 50);
+}
+//move();
+
+//var tr = setInterval(move, 50);
+
+document.getElementById('stop').onclick = function () {
+    //    clearInterval(tr);
+    clearTimeout(timer);
+}
+
+//setTimeout(move, 3000);
+
+var a = 10;
+
+function obr() {
+    document.getElementById('out').innerHTML = a;
+    a--;
+    if (a == -1) {
+        clearTimeout(timer);
+    } else {
+        timer = setTimeout(obr, 1000);
+    }
+}
+
+obr();
