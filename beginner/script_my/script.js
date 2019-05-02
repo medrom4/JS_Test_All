83fe933cb01af18ad2efbda4f31796dc3bcc946f
medrom4 <medikroma@gmail.com>
2019-05-02 13:51:01 +0300

    pusto

--- beginner/script_my/script.js
+++ beginner/script_my/script.js
@@ -1,9 +1,1 @@
-document.getElementById('myslide').onmousemove = function (event) {
-    var x = event.offsetX;
 
-    document.getElementById('two').style.width = x + 'px';
-}
-
-document.getElementById('myslide').onmouseleave = function (event) {
-    document.getElementById('two').style.width = '375px';
-}
