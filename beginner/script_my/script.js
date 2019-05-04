cce4618aa6d774e569de2883ca2587169d79a6cb
medrom4 <medikroma@gmail.com>
2019-05-04 13:44:02 +0300

    watch

--- beginner/script_my/script.js
+++ beginner/script_my/script.js
@@ -1,4 +1,19 @@
 (() => {
+    setInterval(fSec, 200);
+    var sec = 0;
+    var min = 0;
 
+    function fSec() {
+        document.querySelector('#sec').style.transform = 'rotate(' + sec + 'deg)';
+
+        document.querySelector('#min').style.transform = 'rotate(' + min + 'deg)';
+
+        if (sec + 6 == 366) {
+            sec = 0;
+            min += 6;
+        }
+
+        sec += 6;
+    }
 
 })();
