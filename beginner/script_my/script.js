ee4d1c8361a6db2ca6b223f26a601cc3969c1190
medrom4 <medikroma@gmail.com>
2019-05-04 12:52:12 +0300

    pusto

--- beginner/script_my/script.js
+++ beginner/script_my/script.js
@@ -1,18 +1,4 @@
 (() => {
 
-    document.querySelector('#menu').onmouseover = menuShow;
-    document.querySelector('#menu').onmouseout = menuHide;
 
-    document.onkeydown = function (event) {
-        if (event.code == 'Escape') menuHide();
-        if (event.code == 'KeyM') menuShow();
-    }
-
-    function menuShow() {
-        document.querySelector('#menu').style.left = 0;
-    }
-
-    function menuHide() {
-        document.querySelector('#menu').style.left = '-235px';
-    }
 })();
