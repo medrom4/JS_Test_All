d460c9001df8b790928ceb104c836afe85a022f5
medrom4 <medikroma@gmail.com>
2019-05-04 23:27:00 +0300

    Desktop&Mobile

--- beginner/script_my/script.js
+++ beginner/script_my/script.js
@@ -1,4 +1,13 @@
 window.onload = function () {
 
+    // определение устройства
+    document.querySelector('#out').innerHTML = navigator.userAgent;
 
+    console.log(navigator);
+
+    if (navigator.userAgent.match('iPhone') || navigator.userAgent.match('Android') || navigator.userAgent.match('RIM')) {
+        alert('Mobile');
+    } else {
+        alert('DeskTop');
+    }
 }
