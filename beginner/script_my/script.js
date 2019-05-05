0facc37262104de4c2fd7671f87e2ee5c2fbcfbd
medrom4 <medikroma@gmail.com>
2019-05-05 22:50:31 +0300

    PUSTO

--- beginner/script_my/script.js
+++ beginner/script_my/script.js
@@ -1,13 +1,5 @@
 window.onload = function () {
 
-    // определение устройства
-    document.querySelector('#out').innerHTML = navigator.userAgent;
+	
 
-    console.log(navigator);
-
-    if (navigator.userAgent.match('iPhone') || navigator.userAgent.match('Android') || navigator.userAgent.match('RIM')) {
-        alert('Mobile');
-    } else {
-        alert('DeskTop');
-    }
 }
