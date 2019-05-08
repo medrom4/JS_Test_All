667abfeec48786691f1f1a417cc50abfce4f11c1
medrom4 <medikroma@gmail.com>
2019-05-08 16:27:37 +0300

    type-of

--- beginner/script_my/script.js
+++ beginner/script_my/script.js
@@ -1,1 +1,14 @@
+let a = 8;
+console.log(typeof a);
 
+let b = "Hello";
+console.log(typeof b);
+
+let c = 8.2;
+console.log(typeof c);
+
+let d = true;
+console.log(typeof d);
+
+let e = [1,2,3,4,5];
+console.log(typeof e);
