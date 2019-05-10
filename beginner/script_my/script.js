835b7e26ceb53948aba0f429ae969e37831f98fa
medrom4 <medikroma@gmail.com>
2019-05-11 00:23:35 +0300

    function-arguments

--- beginner/script_my/script.js
+++ beginner/script_my/script.js
@@ -1,25 +1,55 @@
-let myFunc = (z, q) => z * 2 * q;
-console.log(myFunc(2, 19));
-//---------------------------
-console.log('---------------------------');
-//---------------------------
+//function f1() {
+//	let div = document.getElementById('one');
+//	div.onclick = function () {
+//		div.innerHTML += " - молодец";
+//	}
+//}
+//f1();
+//
+//function f2() {
+//	let div = document.getElementById('two');
+//	div.onclick = function () {
+//		div.innerHTML += " - молодец 2 часть";
+//	}
+//}
+//f2();
+//
+//let con = document.querySelector('#three');
+//
+//function f3() {
+//	con.onclick = function () {
+//		con.innerHTML += ' молодец часть 3-я';
+//	}
+//}
+//f3();
+//
+//con = document.querySelector('#foure');
+//f3();
 
-let a = (d, c) => {
-	//
-	//
-	return c;
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+
+function f4(zzz) {
+	let d = document.querySelector("#" + zzz);
+	d.onclick = function () {
+		d.innerHTML += " - всё получилось!";
+	}
+}
+f4('one');
+f4('three');
+f4('two');
+f4('foure');
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+
+function summa(a, b) {
+	(a) ? a = a: a = 92;
+	(b) ? b = b: b = 9;
+	return a + b;
 }
 
-let b = () => {
-	//
-	//
-	return true;
-}
-
-let l = c => {
-	//
-	//
-	return c;
-}
-
-let m = (d, c) => d * c;
+console.log(summa(2, 13));
+console.log(summa());
