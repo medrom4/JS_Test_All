901ea62321f4e0126f1cc3869517e951d44e27a0
medrom4 <medikroma@gmail.com>
2019-05-10 21:26:21 +0300

    function

--- beginner/script_my/script.js
+++ beginner/script_my/script.js
@@ -1,1 +1,17 @@
+let numberOne = 4;
 
+function showTemp() {
+	if (numberOne > 0) console.log('больше нуля!');
+	else if (numberOne < 0) console.log('меньше нуля!');
+	else console.log('равно нулю!');
+}
+showTemp();
+
+function summa(a = 2, b = 19) {
+	console.log(a + b);
+	console.log('Температура воды сейчас: ' + numberOne);
+	return a + b;
+}
+summa(2, 14);
+
+console.log(3 * summa(2, 14));
