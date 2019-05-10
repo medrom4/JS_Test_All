0634303ae8859d42a99b00ec5a310ee69812d412
medrom4 <medikroma@gmail.com>
2019-05-10 21:42:36 +0300

    function-vidi

--- beginner/script_my/script.js
+++ beginner/script_my/script.js
@@ -1,17 +1,14 @@
-let numberOne = 4;
-
-function showTemp() {
-	if (numberOne > 0) console.log('больше нуля!');
-	else if (numberOne < 0) console.log('меньше нуля!');
-	else console.log('равно нулю!');
+function getSumm() {
+	console.log(3 * 7);
 }
-showTemp();
+getSumm();
 
-function summa(a = 2, b = 19) {
-	console.log(a + b);
-	console.log('Температура воды сейчас: ' + numberOne);
-	return a + b;
+
+let w = function () {
+	console.log(3 * 6);
 }
-summa(2, 14);
+w();
 
-console.log(3 * summa(2, 14));
+(function getSumm() {
+	console.log(3 * 5);
+})();
