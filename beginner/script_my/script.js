83daea44501a079b80d2feba0497317e5fb65fb2
medrom4 <medikroma@gmail.com>
2019-05-10 20:37:14 +0300

    pusto

--- beginner/script_my/script.js
+++ beginner/script_my/script.js
@@ -1,9 +1,1 @@
-let arr = [20, 4, -41, 54];
 
-let a = arr.reduce(function (x, y) {
-	console.log('первая переменная в функции: ' + x);
-	console.log('вторая переменная в функции: ' + y);
-	return x + y;
-});
-
-console.log(a);
