1ad4f1e14b405f79be69d73be981ba9221f93740
medrom4 <medikroma@gmail.com>
2019-05-11 20:41:54 +0300

    zamikaniia

--- beginner/script_my/script.js
+++ beginner/script_my/script.js
@@ -1,17 +1,15 @@
-//function onClick() {
-//    console.log('ckick');
-//}
-//document.querySelector('#one').onclick = onClick;
-
-document.onclick = function (event) {
-    if (event.target.id == 'one') console.log('Кликнули по "<div> - one"');
+function cs2() {
+    let c = -5;
+    return function(){
+        c++;
+        console.log(c);
+    }
 }
 
-document.querySelector('.two').onclick = function () {
-    console.log('Кликнули по "<div> - two"');
-}
-
-document.querySelector('.three').onclick = function () {
-    //    console.log('Кликнули по "<div> - three"');
-    this.style.display = 'none';
-}
+let d = cs2();
+d();
+d();
+d();
+d();
+d();
+d();
