b38d0e2e13c597889cf2108e063aaca926ec4c02
medrom4 <medikroma@gmail.com>
2019-06-05 15:14:06 +0300

    if-elseif

--- beginner/script_my/script.js
+++ beginner/script_my/script.js
@@ -1,9 +1,10 @@
 document.getElementById('check').onclick = function () {
-	let login = document.getElementById('login').value;
-	let password = document.getElementById('password').value;
+	let age = document.getElementById('age').value;
+	age = parseInt(age);
 
-	if (login == 'ivan' && password == '334455') alert("Приветствуем Вас!");
-	else if (login == 'alex' && password == '777') alert("Приветствуем Вас!");
-	else if (login == 'petr' && password == 'b5678') alert("Приветствуем Вас!");
-	else alert("Вход запрещён!");
+	if (age >= 0 && age < 3) alert("Ваша надбавка - 0%");
+	else if (age >= 3 && age < 10) alert("Ваша надбавка - 10%");
+	else if (age >= 10 && age < 20) alert("Ваша надбавка - 20%");
+	else if (age >= 20 && age < 50) alert("Ваша надбавка - 25%");
+	else alert("Введите корректные данные");
 }
