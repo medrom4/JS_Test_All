37c8eff5aaad4071d674bf338582766837ad9866
medrom4 <medikroma@gmail.com>
2019-06-05 15:00:18 +0300

    if-elseif

--- beginner/script_my/script.js
+++ beginner/script_my/script.js
@@ -1,7 +1,9 @@
-const foo = Object.freeze({
-    'd': 33
-});
-console.log(foo.d);
+document.getElementById('check').onclick = function () {
+	let login = document.getElementById('login').value;
+	let password = document.getElementById('password').value;
 
-foo.d = 44;
-console.log(foo.d);
+	if (login == 'ivan' && password == '334455') alert("Приветствуем Вас!");
+	else if (login == 'alex' && password == '777') alert("Приветствуем Вас!");
+	else if (login == 'petr' && password == 'b5678') alert("Приветствуем Вас!");
+	else alert("Вход запрещён!");
+}
