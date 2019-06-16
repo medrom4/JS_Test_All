c8b5b01049cc0984f23908b805b63b47f2c6644d
medrom4 <medikroma@gmail.com>
2019-06-16 16:50:22 +0300

    sortirovka-arr

--- beginner/script_my/script.js
+++ beginner/script_my/script.js
@@ -1,7 +1,34 @@
-let out = document.getElementById('out');
-let sum = 1;
+let size = ['15px', '12pt', '34em', '-3px', '2%'];
 
-for (var i = 1; i <= 50; i++) {
-	sum *= i;
-	console.log(sum);
+function toNum(arr) {
+	let out = [];
+
+	for (i = 0; i < arr.length; i++) {
+		arr[i] = parseFloat(arr[i]);
+		if (arr[i] > 0) out.push(arr[i])
+	}
+	out.sort(function (a, b) {
+		return a - b;
+	});
+
+	return out;
 }
+
+
+function toNum2(arr) {
+	let newArr = arr.map(function (sz) {
+		return parseFloat(sz);
+	});
+
+	newArr = newArr.filter(function (sz) {
+		if (sz > 0) return sz;
+	});
+
+	newArr.sort(function (a, b) {
+		return a - b;
+	});
+
+	return newArr;
+}
+
+console.log(toNum2(size));
